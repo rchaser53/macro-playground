0bbb01874442b81f630fef5bfc137e23be5f5b77
rchaser53 <tayoshizawa29@gmail.com>
2018-07-01 14:09:38 +0900

    try to make cyclic dependency

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,52 @@
 #[macro_use]
 mod macro_test;
 
+use std::rc::{Rc, Weak};
+use std::error::Error;
+
+struct Part {
+  id: usize,
+  parent: Weak<Part>,
+  children: Vec<Rc<Part>>,
+}
+
+impl Part {
+  // fn new() -> Result<Rc<Part>, Error> {
+  fn new() -> Result<Part, Rc<Part>> {
+
+    let mut parent = Part {
+      id: 1,
+      parent: Weak::new(),
+      children: Vec::new(),
+    };
+
+    let mut child = Part {
+      id: 2,
+      parent: Weak::new(),
+      children: Vec::new(),
+    };
+    let rc_child = Rc::new(child);
+    parent.children.push(rc_child.clone());
+    child.parent = Rc::downgrade(&Rc::new(parent));
+
+    Rc::try_unwrap(rc_child)
+  }
+}
+
 fn main() {
-    println!("{}", sum!(1, 2, 33, 54));
-    println!("{}", count!(1, 5, 2, 33, 54));
-    println!("{}", average!(1, 2, 33, 48));
+  
+
+    // println!("{}", i32 == i32);
 }
 
 
+
+    // .get().unwrap()
+    // child
+      // let mut rc_child = parent.children.get_mut(0).unwrap();
+      // let mut child = Rc::get_mut(rc_child).unwrap();
+      // child.parent = Rc::downgrade(&Rc::new(parent));
+    
+    // child.parent = Rc::downgrade(&Rc::new(parent));
+    // parent: Rc::downgrade(&Rc::new(parent)),
+    // parent
