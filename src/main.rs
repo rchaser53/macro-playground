434cc0c44e94b43960b1774845b4563cc5336f23
rchaser53 <tayoshizawa29@gmail.com>
2018-07-01 15:51:36 +0900

    temp

--- src/main.rs
+++ src/main.rs
@@ -12,31 +12,25 @@ struct Part {
 }
 
 impl Part {
-  fn new() -> Result<Part, Rc<Part>> {
-    let parent = Part {
+  fn new() -> Part {
+    Part {
       id: 1,
       parent: Weak::new(),
       children: Vec::new(),
-    };
-    let rc_parent = Rc::new(parent);
-    let mut parent_copy = rc_parent.clone();
+    }
+  }
 
-    let child = Part {
-      id: 2,
-      parent: Rc::downgrade(&rc_parent),
-      children: Vec::new(),
-    };
-    let rc_child = Rc::new(child);
-    Rc::make_mut(&mut parent_copy).children.push(rc_child.clone());
-    
-    Rc::try_unwrap(parent_copy)
-    // die parent so will get None
+  fn add_child(mut self, mut part: Part) {
+    self.children.push(Rc::new(part));
+    let rc_parent = &Rc::new(self);
+    let weak_parent = Rc::downgrade(&rc_parent);
+    part.parent = weak_parent;    
   }
 }
 
 fn main() {
-  let parent = Part::new().unwrap();
-  println!("{:?}", parent.children[0].parent.upgrade());
+  let parent = Part::new();
+  parent.add_child(Part::new());
 }
 
 /* succeed but it's completely difference what i want */
